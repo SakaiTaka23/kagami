a1c3980ec05e850a38448a18e1c02bed11868f1b
SakaiTaka23 <tt3.sakai@gmail.com>
2022-04-23 09:46:19 +0900

    フォローのみのタイムライン取得

--- backend/src/posts/posts.module.ts
+++ backend/src/posts/posts.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { FollowsService } from 'src/follows/follows.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 import { PostsResolver } from './posts.resolver';
 import { PostsService } from './posts.service';
 
 @Module({
-  providers: [PostsResolver, PostsService, PrismaService],
+  providers: [PostsResolver, PostsService, FollowsService, PrismaService],
 })
 export class PostsModule {}
--- backend/src/posts/posts.resolver.ts
+++ backend/src/posts/posts.resolver.ts
@@ -1,15 +1,21 @@
+import { UseGuards } from '@nestjs/common';
 import { Args, Query, Resolver } from '@nestjs/graphql';
+import { CurrentUserID } from 'src/auth/current-user.decorator';
+import { FirebaseAuthGuard } from 'src/auth/firebase-auth.guard';
+import { FollowsService } from 'src/follows/follows.service';
 
 import { PostsService } from './posts.service';
 
 @Resolver('Post')
 export class PostsResolver {
-  constructor(private readonly postsService: PostsService) {}
+  constructor(private readonly postsService: PostsService, private readonly followsService: FollowsService) {}
 
+  @UseGuards(FirebaseAuthGuard)
   @Query('timeline')
-  getPosts(@Args('take') take: number, @Args('cursor') cursor?: string) {
+  async getPosts(@CurrentUserID() id: string, @Args('take') take: number, @Args('cursor') cursor?: string) {
     const cursorObj = cursor === undefined ? undefined : { id: cursor };
-    return this.postsService.getMany(take, cursorObj);
+    const following = await this.followsService.follows(id);
+    return this.postsService.getMany(id, following, take, cursorObj);
   }
 
   @Query('postDetail')
