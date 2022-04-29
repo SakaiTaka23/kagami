510950a1f74ab9dabf4e87159a98d8fa11092234
SakaiTaka23 <tt3.sakai@gmail.com>
2022-04-29 20:05:50 +0900

    ディバイダー追加

--- frontend/src/components/Posts/Post.tsx
+++ frontend/src/components/Posts/Post.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-import { Grid, ListItem, ListItemText, Typography } from '@mui/material';
+import { Divider, Grid, ListItem, ListItemText, Typography } from '@mui/material';
 import Link from 'next/link';
 
 type Props = {
@@ -16,25 +16,28 @@ const Post: FC<Props> = (post) => {
   const { accountName, userName } = post.user;
   const { id, content } = post;
   return (
-    <ListItem>
-      <ListItemText
-        primary={
-          <Link href={`/${userName}`} passHref>
-            <Grid container>
-              <Typography component='span' variant='subtitle1'>
-                {accountName}
-              </Typography>
-              <Typography component='span' variant='subtitle1' color='gray'>{`@${userName}`}</Typography>
-            </Grid>
-          </Link>
-        }
-        secondary={
-          <Link href={`/${userName}/${id}`} passHref>
-            <Typography component='span'>{content}</Typography>
-          </Link>
-        }
-      />
-    </ListItem>
+    <>
+      <ListItem>
+        <ListItemText
+          primary={
+            <Link href={`/${userName}`} passHref>
+              <Grid container>
+                <Typography component='span' variant='subtitle1'>
+                  {accountName}
+                </Typography>
+                <Typography component='span' variant='subtitle1' color='gray'>{`@${userName}`}</Typography>
+              </Grid>
+            </Link>
+          }
+          secondary={
+            <Link href={`/${userName}/${id}`} passHref>
+              <Typography component='span'>{content}</Typography>
+            </Link>
+          }
+        />
+      </ListItem>
+      <Divider />
+    </>
   );
 };
 
