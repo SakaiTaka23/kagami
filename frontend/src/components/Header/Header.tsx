132d0959558e052b6563529a19a76ea27f88a0fd
SakaiTaka23 <tt3.sakai@gmail.com>
2022-04-30 22:13:26 +0900

    ヘッダーのリンクが動くようにした

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,26 +2,48 @@ import React from 'react';
 
 import { AppBar, Button, Container, MenuItem, Toolbar, Typography } from '@mui/material';
 import { Box } from '@mui/system';
+import { useRouter } from 'next/router';
+
+const title = 'Kagami';
 
 const Header = () => {
+  const router = useRouter();
+  const handlePage = (page: string) => {
+    router.replace(`/${page}`);
+  };
+
   return (
     <AppBar position='static' sx={{ mb: 3 }}>
       <Container maxWidth='xl'>
         <Toolbar disableGutters>
-          <Typography variant='h6' noWrap component='div' sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}>
-            LOGO
+          <Typography
+            variant='h6'
+            onClick={() => handlePage('')}
+            noWrap
+            component='div'
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+          >
+            {title}
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <MenuItem key='New'>
-              <Typography textAlign='center'>New</Typography>
+              <Typography textAlign='center' onClick={() => handlePage('new')}>
+                New
+              </Typography>
             </MenuItem>
           </Box>
           {/* size */}
-          <Typography variant='h6' noWrap component='div' sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            LOGO
+          <Typography
+            variant='h6'
+            onClick={() => handlePage('')}
+            noWrap
+            component='div'
+            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+          >
+            {title}
           </Typography>
           <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
-            <Button key='New' sx={{ my: 2, color: 'white', display: 'block' }}>
+            <Button key='New' onClick={() => handlePage('new')} sx={{ my: 2, color: 'white', display: 'block' }}>
               New
             </Button>
           </Box>
