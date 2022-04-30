6a2955c8c3384b291d0d19295b6c762fa265c71a
SakaiTaka23 <tt3.sakai@gmail.com>
2022-04-30 18:05:31 +0900

    日付が正しいフォーマットで出るように修正

--- backend/src/scalar/DateScalar.ts
+++ backend/src/scalar/DateScalar.ts
@@ -2,15 +2,21 @@ import { Scalar, CustomScalar } from '@nestjs/graphql';
 import { Kind, ValueNode } from 'graphql';
 
 @Scalar('Date')
-export class DateScalar implements CustomScalar<number, Date> {
+export class DateScalar implements CustomScalar<string, Date> {
   description = 'Date custom scalar type';
 
-  parseValue(value: number): Date {
+  parseValue(value: string): Date {
     return new Date(value); // value from the client
   }
 
-  serialize(value: Date): number {
-    return value.getTime(); // value sent to the client
+  serialize(value: Date): string {
+    return `${value.getFullYear()}-${(value.getMonth() + 1).toString().padStart(2, '0')}-${value
+      .getDate()
+      .toString()
+      .padStart(2, '0')}T${value.getHours().toString().padStart(2, '0')}:${value
+      .getMinutes()
+      .toString()
+      .padStart(2, '0')}`;
   }
 
   parseLiteral(ast: ValueNode): Date {
