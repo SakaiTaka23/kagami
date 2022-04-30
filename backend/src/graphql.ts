230da66437b5355dc673786d2f8e9a6a94fb111f
SakaiTaka23 <tt3.sakai@gmail.com>
2022-04-30 11:43:43 +0900

    日付を返せるようにした

--- backend/src/graphql.ts
+++ backend/src/graphql.ts
@@ -1,3 +1,4 @@
+
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -7,51 +8,52 @@
 /* tslint:disable */
 /* eslint-disable */
 export class UserName {
-  accountName: string;
-  userName: string;
+    accountName: string;
+    userName: string;
 }
 
 export class Follow {
-  followerId: string;
-  followingId: string;
+    followerId: string;
+    followingId: string;
 }
 
 export abstract class IQuery {
-  abstract isFollowing(userName: string): boolean | Promise<boolean>;
+    abstract isFollowing(userName: string): boolean | Promise<boolean>;
 
-  abstract timeline(take: number, cursor?: Nullable<string>): Post[] | Promise<Post[]>;
+    abstract timeline(take: number, cursor?: Nullable<string>): Post[] | Promise<Post[]>;
 
-  abstract postUser(userName: string, take: number, cursor?: Nullable<string>): Post[] | Promise<Post[]>;
+    abstract postUser(userName: string, take: number, cursor?: Nullable<string>): Post[] | Promise<Post[]>;
 
-  abstract postDetail(id: string, userName: string): Nullable<Post> | Promise<Nullable<Post>>;
+    abstract postDetail(id: string, userName: string): Nullable<Post> | Promise<Nullable<Post>>;
 
-  abstract userFromToken(): User | Promise<User>;
+    abstract userFromToken(): User | Promise<User>;
 
-  abstract userFromUserName(userName: string): Nullable<User> | Promise<Nullable<User>>;
+    abstract userFromUserName(userName: string): Nullable<User> | Promise<Nullable<User>>;
 
-  abstract user(id: string): Nullable<User> | Promise<Nullable<User>>;
+    abstract user(id: string): Nullable<User> | Promise<Nullable<User>>;
 }
 
 export abstract class IMutation {
-  abstract followToggle(userName: string): Follow | Promise<Follow>;
+    abstract followToggle(userName: string): Follow | Promise<Follow>;
 
-  abstract postCreate(content: string): Post | Promise<Post>;
+    abstract postCreate(content: string): Post | Promise<Post>;
 
-  abstract createUser(username: UserName): User | Promise<User>;
+    abstract createUser(username: UserName): User | Promise<User>;
 }
 
 export class Post {
-  id: string;
-  content: string;
-  user: User;
-  userId: string;
+    id: string;
+    content: string;
+    createdAt: string;
+    user: User;
+    userId: string;
 }
 
 export class User {
-  id: string;
-  accountName: string;
-  userName: string;
-  profile: string;
+    id: string;
+    accountName: string;
+    userName: string;
+    profile: string;
 }
 
 type Nullable<T> = T | null;
