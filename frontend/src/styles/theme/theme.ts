269968ce75f2a571c058d67f48e60c29c19f1286
SakaiTaka23 <tt3.sakai@gmail.com>
2022-05-01 10:51:24 +0900

    テーマ変更

--- frontend/src/styles/theme/theme.ts
+++ frontend/src/styles/theme/theme.ts
@@ -1,7 +1,11 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
-  // TODO:テーマ設定を行います
+  palette: {
+    primary: {
+      main: '#e60000',
+    },
+  },
 });
 
 export default theme;
