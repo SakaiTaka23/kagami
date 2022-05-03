f05824ca2aacfb98485e7ec2fe4ca157f45fe8b1
SakaiTaka23 <tt3.sakai@gmail.com>
2022-05-03 14:49:25 +0900

    本人はフォローできないように処理

--- backend/src/follows/follows.resolver.ts
+++ backend/src/follows/follows.resolver.ts
@@ -1,6 +1,5 @@
 import { HttpException, HttpStatus, UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { Prisma } from '@prisma/client';
 import { CurrentUserID } from 'src/auth/current-user.decorator';
 import { FirebaseAuthGuard } from 'src/auth/firebase-auth.guard';
 import { OptionalAuthGuard } from 'src/auth/optional-auth.guard';
@@ -24,14 +23,12 @@ export class FollowsResolver {
   async toggle(@CurrentUserID() id: string, @Args('userName') userName: string) {
     const isFollowing = await this.followsService.isFollowing(id, userName);
     const user = await this.usersService.fromUserName(userName);
+    if (id === user.id) {
+      // user follows them self
+      throw new HttpException('BadRequest', HttpStatus.BAD_REQUEST);
+    }
     if (isFollowing === 0) {
-      return this.followsService.follow(id, user.id).catch((e) => {
-        if (e instanceof Prisma.PrismaClientKnownRequestError) {
-          if (e.code === 'P2003') {
-            throw new HttpException('BadRequest', HttpStatus.BAD_REQUEST);
-          }
-        }
-      });
+      return this.followsService.follow(id, user.id);
     }
     return this.followsService.unFollow(id, user.id);
   }
