5d2e2bbd368eb7c84c2cf7d0ef95c3eec470c50a
SakaiTaka23 <tt3.sakai@gmail.com>
2022-05-04 19:12:03 +0900

    該当ユーザー以外は弾くようにした

--- frontend/src/pages/[username]/edit.tsx
+++ frontend/src/pages/[username]/edit.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { useRouter } from 'next/router';
 
 import ProfileEdit from '@/components/Profile/EditForm';
+import { AuthContext } from '@/firebase/authContext';
 import { useEditProfileQuery } from '@/graphql/generated';
 
 const EditProfile = () => {
-  const { data, loading, error } = useEditProfileQuery();
+  const { userName, isLoading } = useContext(AuthContext);
+  const router = useRouter();
+  const { data, loading, error } = useEditProfileQuery({
+    skip: !router.isReady,
+  });
 
-  if (loading) return <p>Loading...</p>;
+  if (loading || isLoading || !router.isReady) return <p>Loading...</p>;
+  if (userName !== router.query.username) {
+    router.push('/404');
+  }
   if (error) return <p>Error</p>;
 
   return <>{data?.userFromToken && <ProfileEdit {...data?.userFromToken} />}</>;
