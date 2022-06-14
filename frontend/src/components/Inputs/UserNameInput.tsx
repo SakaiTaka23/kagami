5518aa1d65a6921dbc83736f5ae019ed2be6b1e0
SakaiTaka23 <tt3.sakai@gmail.com>
2022-06-14 21:03:24 +0900

    インプットに反映

--- frontend/src/components/Inputs/UserNameInput.tsx
+++ frontend/src/components/Inputs/UserNameInput.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
 import { TextField, Typography } from '@mui/material';
 import { Controller, useFormContext } from 'react-hook-form';
 
+import { useUserUniqueLazyQuery } from '@/graphql/generated';
+
 const UserNameInput = () => {
+  const [UserUniqueQuery] = useUserUniqueLazyQuery();
   const {
     control,
+    clearErrors,
+    setError,
+    trigger,
     formState: { errors },
   } = useFormContext();
+
+  const changeName = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    UserUniqueQuery({
+      variables: {
+        userName: e.target.value,
+      },
+    }).then((res) => {
+      if (res.data?.userFromUserName?.id !== undefined) {
+        setError('userName', { message: 'This username is already used' });
+      } else {
+        trigger('userName').then((isValid) => {
+          if (isValid) {
+            clearErrors('userName');
+          }
+        });
+      }
+    });
+  };
 
   return (
     <>
@@ -16,14 +40,28 @@ const UserNameInput = () => {
         defaultValue=''
         name='userName'
         rules={{
-          required: true,
-          pattern: /^[0-9a-zA-Z_]{1,15}$/,
+          required: 'required and must be 1 to 16 long \n only number alphabet and underscores',
+          pattern: {
+            value: /^[0-9a-zA-Z_]{1,15}$/,
+            message: 'required and must be 1 to 16 long \n only number alphabet and underscores',
+          },
         }}
-        render={({ field }) => <TextField {...field} margin='normal' placeholder='User Name *' fullWidth />}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            onChange={(e) => {
+              field.onChange(e);
+              changeName(e);
+            }}
+            margin='normal'
+            placeholder='User Name *'
+            fullWidth
+          />
+        )}
       />
       {errors.userName && (
         <Typography color='error' variant='overline'>
-          {`required and must be 1 to 16 long \n only number alphabet and underscores`}
+          {errors.userName.message}
         </Typography>
       )}
     </>
