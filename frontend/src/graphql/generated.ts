cba4f08b7ef5f75649ae2ac07e3dddfafb14bdac
SakaiTaka23 <tt3.sakai@gmail.com>
2022-06-14 21:03:17 +0900

    ユーザーのユニーク保証を行うクエリ記述

--- frontend/src/graphql/generated.ts
+++ frontend/src/graphql/generated.ts
@@ -188,6 +188,13 @@ export type UserProfileQueryVariables = Exact<{
 
 export type UserProfileQuery = { __typename?: 'Query', isFollowing: boolean, userFromUserName?: { __typename?: 'User', accountName: string, userName: string, profile: string } | null, postUser: Array<{ __typename?: 'Post', id: string, content: string, createdAt: any, user: { __typename?: 'User', accountName: string, userName: string } }> };
 
+export type UserUniqueQueryVariables = Exact<{
+  userName: Scalars['String'];
+}>;
+
+
+export type UserUniqueQuery = { __typename?: 'Query', userFromUserName?: { __typename?: 'User', id: string } | null };
+
 export type EditProfileQueryVariables = Exact<{ [key: string]: never; }>;
 
 
@@ -531,6 +538,41 @@ export function useUserProfileLazyQuery(baseOptions?: Apollo.LazyQueryHookOption
 export type UserProfileQueryHookResult = ReturnType<typeof useUserProfileQuery>;
 export type UserProfileLazyQueryHookResult = ReturnType<typeof useUserProfileLazyQuery>;
 export type UserProfileQueryResult = Apollo.QueryResult<UserProfileQuery, UserProfileQueryVariables>;
+export const UserUniqueDocument = gql`
+    query UserUnique($userName: String!) {
+  userFromUserName(userName: $userName) {
+    id
+  }
+}
+    `;
+
+/**
+ * __useUserUniqueQuery__
+ *
+ * To run a query within a React component, call `useUserUniqueQuery` and pass it any options that fit your needs.
+ * When your component renders, `useUserUniqueQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useUserUniqueQuery({
+ *   variables: {
+ *      userName: // value for 'userName'
+ *   },
+ * });
+ */
+export function useUserUniqueQuery(baseOptions: Apollo.QueryHookOptions<UserUniqueQuery, UserUniqueQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<UserUniqueQuery, UserUniqueQueryVariables>(UserUniqueDocument, options);
+      }
+export function useUserUniqueLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<UserUniqueQuery, UserUniqueQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<UserUniqueQuery, UserUniqueQueryVariables>(UserUniqueDocument, options);
+        }
+export type UserUniqueQueryHookResult = ReturnType<typeof useUserUniqueQuery>;
+export type UserUniqueLazyQueryHookResult = ReturnType<typeof useUserUniqueLazyQuery>;
+export type UserUniqueQueryResult = Apollo.QueryResult<UserUniqueQuery, UserUniqueQueryVariables>;
 export const EditProfileDocument = gql`
     query EditProfile {
   userFromToken {
