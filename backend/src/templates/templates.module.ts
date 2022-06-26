31e092e3d774caef37bb2e0af6a8f72d5d332dcc
SakaiTaka23 <tt3.sakai@gmail.com>
2022-06-26 10:38:40 +0900

    投稿用のミューテーション追加

--- backend/src/templates/templates.module.ts
+++ backend/src/templates/templates.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 import { TemplatesResolver } from './templates.resolver';
 import { TemplatesService } from './templates.service';
 
 @Module({
-  providers: [TemplatesResolver, TemplatesService],
+  providers: [TemplatesResolver, TemplatesService, PrismaService],
 })
 export class TemplatesModule {}
--- backend/src/templates/templates.service.ts
+++ backend/src/templates/templates.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class TemplatesService {}
+export class TemplatesService {
+  constructor(private prisma: PrismaService) {}
+
+  create(id: string, content: string, detail: string) {
+    return this.prisma.template.create({
+      data: {
+        content,
+        detail,
+        userId: id,
+      },
+      include: {
+        user: true,
+      },
+    });
+  }
+}
