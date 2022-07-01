b08cf4f63e4db93e6dd30fafda985397b54cfd51
SakaiTaka23 <tt3.sakai@gmail.com>
2022-07-01 09:59:31 +0900

    雛形だけ出力できるようにした

---
+++ frontend/src/components/Templates/Templates.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react';
+
+import { List } from '@mui/material';
+
+import Template from './Template';
+
+type Props = {
+  templates: {
+    id: string;
+    content: string;
+    createdAt: string;
+    updatedAt: string;
+    user: {
+      accountName: string;
+      userName: string;
+    };
+  }[];
+};
+
+const Templates: FC<Props> = (props) => {
+  return (
+    <List sx={{ width: '100%', bgcolor: 'background.color' }}>
+      {props.templates?.map((template, i) => {
+        return <Template key={i} />;
+      })}
+    </List>
+  );
+};
+
+export default Templates;
