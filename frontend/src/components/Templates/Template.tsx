7c3014e43ff97d89ffe520edd2e3ba25e4d3b956
SakaiTaka23 <tt3.sakai@gmail.com>
2022-07-01 10:34:00 +0900

    テンプレートの詳細表示

--- frontend/src/components/Templates/Template.tsx
+++ frontend/src/components/Templates/Template.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { FC } from 'react';
 
-const Template = () => {
-  return <div>Template</div>;
+import { Grid, ListItem, ListItemText, Typography } from '@mui/material';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import Link from 'next/link';
+
+type Props = {
+  id: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  user: {
+    accountName: string;
+    userName: string;
+  };
 };
 
-export default Template;
+export const Template: FC<Props> = (template) => {
+  dayjs.extend(relativeTime);
+
+  const { accountName, userName } = template.user;
+  return (
+    <ListItem>
+      <ListItemText
+        primary={
+          <Link href={`/${userName}`} passHref>
+            <Grid container>
+              <Typography component='span' variant='subtitle1' sx={{ fontWeight: 'bold' }}>
+                {`${accountName} `}
+              </Typography>
+              <Typography component='span' variant='subtitle1' color='gray'>{`@${userName}`}</Typography>
+            </Grid>
+          </Link>
+        }
+        secondary={
+          <Link href={`/templates/${template.id}`} passHref>
+            <Typography component='span'>{template.content}</Typography>
+          </Link>
+        }
+      />
+    </ListItem>
+  );
+};
