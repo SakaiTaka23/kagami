ad5dd8f11136183696d9ea3175bdce38f77a1888
SakaiTaka23 <tt3.sakai@gmail.com>
2022-07-24 14:28:01 +0900

    テンプレート一覧を取得するクエリ記述

---
+++ frontend/src/pages/new/select.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { TemplateSelector } from '@/components/Templates';
+
+const SelectPostStyle = () => {
+  return <TemplateSelector />;
+};
+
+export default SelectPostStyle;
