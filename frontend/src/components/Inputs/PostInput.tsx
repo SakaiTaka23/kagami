25b05de3fae99af5b9a31190d3de079ac2c4892f
SakaiTaka23 <tt3.sakai@gmail.com>
2022-11-25 09:32:02 +0900

    動くように修正

--- frontend/src/components/Inputs/PostInput.tsx
+++ frontend/src/components/Inputs/PostInput.tsx
@@ -2,37 +2,30 @@ import React, { FC } from 'react';
 
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
 import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
 import { HashtagPlugin } from '@lexical/react/LexicalHashtagPlugin';
 import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
 import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin';
-import { $createParagraphNode, $createTextNode, $getRoot, EditorState } from 'lexical';
+import { EditorState } from 'lexical';
 
 import Counter from './assets/Counter';
 import editorConfig from './config/editorConfig';
 
 type Props = {
-  content?: string;
   count: number;
   onChange: (editorState: EditorState) => void;
 };
 
 const maxLength = 140;
 
-export const PostInput: FC<Props> = ({ content, count, onChange }) => {
+export const PostInput: FC<Props> = ({ count, onChange }) => {
   return (
     <LexicalComposer initialConfig={editorConfig}>
       <div className='editor-container'>
         <PlainTextPlugin
           contentEditable={<ContentEditable className='editor-input' />}
-          initialEditorState={() => {
-            const paragraph = $createParagraphNode();
-            const text = $createTextNode(content);
-            const root = $getRoot();
-            paragraph.append(text);
-            root.append(paragraph);
-            root.selectEnd();
-          }}
           placeholder={<div className='editor-placeholder'>Enter some plain text...</div>}
+          ErrorBoundary={LexicalErrorBoundary}
         />
         <OnChangePlugin ignoreSelectionChange={true} onChange={onChange} />
         <HashtagPlugin />
